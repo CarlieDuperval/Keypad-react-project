8c795596c1065f46be3cda007e104c7a735b08d0
Carlie Duperval <carlierichell@gmail.com>
2022-05-13 21:35:48 -0400

    Added first part of  Keypad projet in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  
+  const [string ,setString] = useState('')
+
+  const handleBackButton = () => {
+    setString(string.slice(0, string.length - 1))
+  }
+
+const validateButton = () => {
+  string === "1234" ? setString("Welcome"): setString("Denied")
+}
+
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <section class="w3=container">
+        <h1>Jayden Keypad</h1>
+        <h2>{string}</h2>
+        </section>
+        <section>
+       
+        <button onClick={()=>{
+          if(string.length < 4)
+          setString(string + '1')}}>1</button> 
+        <button onClick={()=>{
+          if(string.length < 4)
+          setString(string + '2')}}>2</button>
+        <button onClick={()=>{
+          if(string.length < 4)
+          setString(string + '3')}}>3</button>
+        
+        </section>
+
+        <section>
+        <button onClick={()=>{
+          if(string.length < 4)
+          setString(string + '4')}}>4</button>
+        <button onClick={()=>{
+          if(string.length < 4)
+          setString(string + '5')}}>5</button>
+        <button onClick={()=>{
+          if(string.length < 4)
+          setString(string + '6')}}>6</button>
+        </section>
+
+        <section>
+        <button onClick={()=>{
+          if(string.length < 4)
+          setString(string + '7')}}>7</button>
+        <button onClick={()=>{
+          if(string.length < 4)
+          setString(string + '8')}}>8</button>
+        <button onClick={()=>{
+          if(string.length < 4)
+          setString(string + '9')}}>9</button>
+        </section>
+
+        <section>
+        <button onClick={handleBackButton}>⬅️</button>
+        <button onClick={()=>{
+          if(string.length < 4)
+          setString(string + '0')}}>0 </button>
+        <button onClick={validateButton}>✅</button>
+        </section>
+        <section>
+        <button onClick={()=>setString(0)}>🔄</button>
+        </section>
+        </header>
     </div>
   );
 }
